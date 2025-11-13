c7c4f4ca270a302383f470ead7058227f46f7682
glopesdev <g.lopes@neurogears.org>
2025-11-13 15:27:13 +0000

    Normalize repository structure and internalize SpikeGLX API (#8)
    * Normalize license file formatting
    * Normalize repository structure
    * Normalize documentation strings and overloads
    * Rename SpikeGLX submodule folder
    * Internalize SpikeGLX C# API
    * Ensure source generator runs when building docs

--- src/Bonsai.SpikeGLX/DigitalOutput.cs
+++ src/Bonsai.SpikeGLX/DigitalOutput.cs
@@ -4,89 +4,67 @@ using System.Reactive.Linq;
 
 namespace Bonsai.SpikeGLX
 {
-    // <summary>
-    /// Represents an operator that controls a digital output.
+    /// <summary>
+    /// Represents an operator that sets one or more digital output lines through SpikeGLX from
+    /// a sequence of values representing the state of the line.
     /// </summary>
     [Combinator]
-    [Description("Controls a digital output.")]
+    [Description("Sets one or more digital output lines through SpikeGLX from a sequence of values representing the state of the line.")]
     [WorkflowElementCategory(ElementCategory.Sink)]
     public class DigitalOutput
     {
         /// <summary>
-        /// Gets or sets the IP address of the SpikeGLX command server
+        /// Gets or sets the IP address of the SpikeGLX command server.
         /// </summary>
-        [Description("IP address of the SpikeGLX command server.")]
+        [Description("The IP address of the SpikeGLX command server.")]
         public string Host { get; set; } = "localhost";
 
         /// <summary>
         /// Gets or sets the port of the SpikeGLX command server.
         /// </summary>
-        [Description("Port of the SpikeGLX command server.")]
+        [Description("The port of the SpikeGLX command server.")]
         public int Port { get; set; } = 4142;
 
         /// <summary>
-        /// Gets or sets the channels to output the digital signal to.
-        /// Strings have format \"Dev6/port0/line2,Dev6/port0/line5\".
+        /// Gets or sets the name of the channels to output the digital signal to.
         /// </summary>
-        [Description("Channels to output the digital signal to. " +
-            "Strings have format \"Dev6/port0/line2,Dev6/port0/line5\"")]
+        /// <remarks>
+        /// Strings have format \"Dev6/port0/line2,Dev6/port0/line5\".
+        /// </remarks>
+        [Description("The name of the channels to output the digital signal to.")]
         public string Channels { get; set; }
 
         /// <summary>
-        /// Set a digital output.
+        /// Sets one or more digital output lines through SpikeGLX from an observable sequence
+        /// of integer values representing a digital state.
         /// </summary>
         /// <param name="source">
-        /// A sequence of <see cref="int"/>s, each representing a high (1) or low (0) digital state.
+        /// A sequence of 32-bit signed integers, where each value represents a high (1) or low (0)
+        /// state to which to set the digital output line.
         /// </param>
         /// <returns>
         /// An observable sequence that is identical to the <paramref name="source"/> sequence
-        /// but where there is an additional side effect of setting a digital output. 
+        /// but where there is an additional side effect of setting digital output lines through
+        /// SpikeGLX.
         /// </returns>
         public IObservable<int> Process(IObservable<int> source)
         {
-            return Observable.Using<int, SpikeGLX>(() => new SpikeGLX(Host, Port),
+            return Observable.Using(() => new SpikeGLX(Host, Port),
                 connection => source.Do(input => connection.SetDigitalOut(input, Channels)));
         }
 
         /// <summary>
-        /// Set a digital output.
+        /// Sets one or more digital output lines through SpikeGLX from an observable sequence
+        /// of boolean values representing a digital state.
         /// </summary>
         /// <param name="source">
-        /// A sequence of <see cref="float"/>s, each representing a high (1.0) or low (0.0) digital state.
+        /// A sequence of boolean values, where each value represents a high (<see langword="true"/>)
+        /// or low (<see langword="false"/>) state to which to set the digital output line.
         /// </param>
         /// <returns>
         /// An observable sequence that is identical to the <paramref name="source"/> sequence
-        /// but where there is an additional side effect of setting a digital output. 
-        /// </returns>
-        public IObservable<int> Process(IObservable<float> source)
-        {
-            return Process(source.Select(input => (int)input));
-        }
-
-        /// <summary>
-        /// Set a digital output.
-        /// </summary>
-        /// <param name="source">
-        /// A sequence of <see cref="float"/>s, each representing a high (1.0) or low (0.0) digital state.
-        /// </param>
-        /// <returns>
-        /// An observable sequence that is identical to the <paramref name="source"/> sequence
-        /// but where there is an additional side effect of setting a digital output. 
-        /// </returns>
-        public IObservable<int> Process(IObservable<double> source)
-        {
-            return Process(source.Select(input => (int)input));
-        }
-
-        /// <summary>
-        /// Set a digital output.
-        /// </summary>
-        /// <param name="source">
-        /// A sequence of <see cref="bool"/>s, each representing a high (true) or low (false) digital state.
-        /// </param>
-        /// <returns>
-        /// An observable sequence that is identical to the <paramref name="source"/> sequence
-        /// but where there is an additional side effect of setting a digital output. 
+        /// but where there is an additional side effect of setting digital output lines through
+        /// SpikeGLX.
         /// </returns>
         public IObservable<int> Process(IObservable<bool> source)
         {
