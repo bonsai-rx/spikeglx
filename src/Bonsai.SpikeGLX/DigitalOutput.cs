562717a063c096e575d832d2931a8ddf1fe5cf1a
Jacob White <jacobmw@mit.edu>
2025-11-26 10:56:57 -0500

    Allow for use of bitmask in DigitalOutput.

--- src/Bonsai.SpikeGLX/DigitalOutput.cs
+++ src/Bonsai.SpikeGLX/DigitalOutput.cs
@@ -5,12 +5,12 @@ using System.Reactive.Linq;
 namespace Bonsai.SpikeGLX
 {
     /// <summary>
-    /// Represents an operator that sets one or more digital output lines through SpikeGLX from
+    /// Represents an operator that sets one or more NI digital output lines through SpikeGLX from
     /// a sequence of values representing the state of the line.
     /// </summary>
     [Combinator]
     [WorkflowElementCategory(ElementCategory.Sink)]
-    [Description("Sets one or more digital output lines through SpikeGLX from a sequence of values representing the state of the line.")]
+    [Description("Sets one or more NI digital output lines through SpikeGLX from a sequence of values representing the state of the line.")]
     public class DigitalOutput
     {
         /// <summary>
@@ -31,18 +31,50 @@ namespace Bonsai.SpikeGLX
         /// Gets or sets the name of the channels to output the digital signal to.
         /// </summary>
         /// <remarks>
-        /// Strings have format \"Dev6/port0/line2,Dev6/port0/line5\".
+        /// Only lines from a single device should be listed. Valid formats include:
+        /// <list type="bullet">
+        /// <item>\"Dev6/port0/line2,Dev6/port0/line5\"</item>
+        /// <item>\"Dev6/port1/line0:3\"</item>
+        /// <item>\"Dev6/port1:2\"</item>
+        /// </list>
         /// </remarks>
         [Description("The name of the channels to output the digital signal to.")]
         public string Channels { get; set; }
 
         /// <summary>
-        /// Sets one or more digital output lines through SpikeGLX from an observable sequence
+        /// Sets one or more NI digital output lines through SpikeGLX from an observable sequence
+        /// of unsigned integer values representing a digital state.
+        /// </summary>
+        /// <param name="source">
+        /// A sequence of 32-bit unsigned integers, where each value represents a bitmask of 
+        /// states to which to set the digital output lines.
+        /// </param>
+        /// <returns>
+        /// An observable sequence that is identical to the <paramref name="source"/> sequence
+        /// but where there is an additional side effect of setting digital output lines through
+        /// SpikeGLX.
+        /// </returns>
+        /// <remarks>
+        /// The lowest 8 bits of the bitmask map to port0, the next higher 8 bits to port 1, etc.
+        /// This mapping is fixed, irrespective of whether only a subset of lines have been
+        /// selected in <see cref="Channels"/>. The effect of selecting a subset of lines
+        /// in <see cref="Channels"/> is to ignore those bits in the bitmask corresponding
+        /// to unlisted lines. 
+        /// </remarks>
+        public IObservable<uint> Process(IObservable<uint> source)
+        {
+            return Observable.Using(() => new SpikeGLX(Host, Port),
+                connection => source.Do(input => connection.SetNIDigitalOut(Channels,
+                input)));
+        }
+
+        /// <summary>
+        /// Sets one or more NI digital output lines through SpikeGLX from an observable sequence
         /// of boolean values representing a digital state.
         /// </summary>
         /// <param name="source">
         /// A sequence of boolean values, where each value represents a high (<see langword="true"/>)
-        /// or low (<see langword="false"/>) state to which to set the digital output line.
+        /// or low (<see langword="false"/>) state to which to set the digital output lines.
         /// </param>
         /// <returns>
         /// An observable sequence that is identical to the <paramref name="source"/> sequence
@@ -54,26 +86,6 @@ namespace Bonsai.SpikeGLX
             return Observable.Using(() => new SpikeGLX(Host, Port),
                 connection => source.Do(input => connection.SetNIDigitalOut(Channels,
                 input ? uint.MaxValue : uint.MinValue)));
-        }
-
-        /// <summary>
-        /// Sets one or more digital output lines through SpikeGLX from an observable sequence
-        /// of integer values representing a digital state.
-        /// </summary>
-        /// <param name="source">
-        /// A sequence of 32-bit signed integers, where each value represents a high (1) or low (0)
-        /// state to which to set the digital output line.
-        /// </param>
-        /// <returns>
-        /// An observable sequence that is identical to the <paramref name="source"/> sequence
-        /// but where there is an additional side effect of setting digital output lines through
-        /// SpikeGLX.
-        /// </returns>
-        public IObservable<int> Process(IObservable<int> source)
-        {
-            return Observable.Using(() => new SpikeGLX(Host, Port),
-                connection => source.Do(input => connection.SetNIDigitalOut(Channels,
-                input != 0 ? uint.MaxValue : uint.MinValue)));
         }
     }
 }
